a9d5b00afb601f486c1b77bba99cb04fa45308ac
Simas Cepaitis <simas.cepaitis@snc.lt>
2019-02-24 12:46:34 +0000

    remove unused function

--- cmd/root.go
+++ cmd/root.go
@@ -3,13 +3,8 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"reflect"
 
-	"github.com/geriBatai/kmode/pkg/kubernetes"
-	"github.com/kubernetes/cli-runtime/pkg/genericclioptions/printers"
 	"github.com/spf13/cobra"
-	lua "github.com/yuin/gopher-lua"
-	"k8s.io/apimachinery/pkg/runtime"
 )
 
 var rootCmd = &cobra.Command{
@@ -31,21 +26,5 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
-}
-
-func marshalValue(val lua.LValue) {
-	ud := val.(*lua.LUserData)
-
-	switch ud.Value.(type) {
-	case kubernetes.Resource:
-		printer := &printers.YAMLPrinter{}
-		fmt.Printf("---\n")
-		err := printer.PrintObj(ud.Value.(runtime.Object), os.Stdout)
-		if err != nil {
-			fmt.Printf("ERROR: %v", err)
-		}
-	default:
-		fmt.Printf("TYPE: %v\n", reflect.TypeOf(ud.Value))
 	}
 }
